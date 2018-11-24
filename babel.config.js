e9cef9f9d78e0c322b98e39ccd1079d46e14b07d
linzebin <443189390@qq.com>
2018-11-24 23:11:59 +0800

    first commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.android.js', '.android.tsx', '.ios.js', '.ios.tsx'],
+          root: ['./src']
+        }
+      ]
+    ]
+  };
+};
